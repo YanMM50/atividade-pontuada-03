6a156fc9d6de1b5b018a6b733dc5c42b28bea9b0
YanMM50 <yanmendes50@gmail.com>
2024-10-01 16:45:17 +0000

    salvar

---
+++ Atividade prefeitura yan.py
@@ -0,0 +1,31 @@
+"""
+A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número de filhos das famílias da cidade. Aprefeitura deseja saber
+
+a) total de famílias que responderam a pesquisa;
+b)média do salário da população;
+c) média do números de filhos;
+d) maior salário;
+e) menor salário.
+
+Crie um menu com as seguintes opções.
+código | descrição
+  1       Adicionar família
+  2       Exibir rsultados
+  3       Sair
+
+Ao adicionar uma família, deve-se limpar o terminal e apresentar o menu novamente.
+1. Salve os dados em um arquivo com nome: pesquisa_prefeitura.txt
+2. O programa deve ler o arquivo para exibir os dados salvos.  
+
+"""
+
+import os
+os.system("cls || clear")
+
+from dataclasses import dataclass
+
+@dataclass
+class Cliente:
+    quantidade_de_filhos = int
+    salario = float
+    
