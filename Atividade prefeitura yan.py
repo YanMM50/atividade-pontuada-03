812263000069bc7d95c74097600f9553419257b8
YanMM50 <yanmendes50@gmail.com>
2024-10-01 19:10:56 +0000

    salvar

--- Atividade prefeitura yan.py
+++ Atividade prefeitura yan.py
@@ -27,8 +27,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Cliente:
-    quantidade_de_filhos = int
-    salario = float
+    quantidade_de_filhos : int
+    salario : float
 
 def menu ():
     print("""
@@ -37,70 +37,93 @@ def menu ():
 1. Adicionar família          
 2. Exibir os resultados          
 3. Sair             
+
           """)
+    
+    
+def criando_arquivo(a,b):
+    with open(a, "a") as lista_clientes:
+        for cliente in b:
+            lista_clientes.write(f"{cliente.quantidade_de_filhos}, {cliente.salario}\n")
+
+
+def lendo_arquivo(a):
+    with open(a, "r") as arquivo_clientes:
+        for linha in arquivo_clientes:
+            quantidade_de_filhos, salario = linha.strip() .split(",")
+            lista_total.append(Cliente(quantidade_de_filhos=int(quantidade_de_filhos), salario=float(salario)))
+    return lista_total
+
+
+
+def media_total(a):
+    QNT =  len(a) 
+    soma = sum(a)
+
+    if QNT == 0:
+        return 0
+    else: 
+        media = soma / QNT
+        return media
+lista_total = []
+
+
+def limpar_tela():
+    os.system("cls || clear ")
+
+contador = 0
+while True: 
+    menu()
+    opcao = (input("Resposta: "))
+    match opcao:
+        case "1":
+            cliente = Cliente(
+            quantidade_de_filhos = int(input("Quantos filhos você tem: ")),
+            salario = float(input("Seu salário: "))
+            )
+            lista_total.append(cliente)
+            nome_arquivo_cliente = "pesquisa_prefeitura.txt"
+            criando_arquivo(nome_arquivo_cliente, lista_total)
+            
+        case "2":
+            contador = 0
+            nome_arquivo_cliente = "pesquisa_prefeitura.txt"
+            lista_salario=[]
+            lista_filhos = []
+            lista_total_definitiva = lendo_arquivo(nome_arquivo_cliente)
+            for cliente in lista_total_definitiva:
+                lista_filhos.append(cliente.quantidade_de_filhos)
+                lista_salario.append(cliente.salario)
+                contador += 1
+            media_salario = media_total(lista_salario)
+            media_filhos = media_total(lista_filhos)
+            maior_salario = max(lista_salario)
+            menor_salario = min(lista_salario) 
+            print("\n==== Resultados ====")
+            print(f"Total de famílias: {contador}")
+            print(f"Média do salário: {media_salario}")
+            print(f"Média do número de filhos: {media_filhos}")
+            print(f"Maior Salário: {maior_salario}")
+            print(f"Menor Salário: {menor_salario}")
+            
+                            
+            
+        case "3":
+            break
+            
+        case _:
+            print("Opção inválida")
+            continue
     
 
 
-def opcoes():
-    menu()
-    opcao = (input("Resposta: "))
-    lista_total = []
-    contador = 0
-    while True:
-        
-        match opcao:
-            case "1":
-                cliente = Cliente(
-            qnt_filhos = int(input("Quantos filhos você tem: ")),
-            salarios = float(input("Seu salário: "))
-        )
-                lista_total.append(cliente)
-                contador += 1
-            case "2":
-                print("\n==== Resultados ====")
-                print(f"Total de famílias: {total_familias}")
-                print(f"Média do salário: {media_salario}")
-                print(f"Média do número de filhos: {media_filhos}")
-                print(f"Maior Salário: {maior_salario}")
-                print(f"Menor Salário: {menor_salario}")
-                                
-                
-            case "3":
-                break
-                
-            case _:
-                print("Opção inválida")
-                continue
-            
-        menu()
-        opcao2 = input("Resposta: ")
+
+
+
 
         
 
 
-    return lista_total, contador
-
-        
-
-
-
-lista_total, contador = opcoes()
-
-
-def lendo_e_mostrando_arquivo():
-    nome_arquivo_cliente = "pesquisa_prefeitura.txt"
-    with open(nome_arquivo_cliente, "w") as lista_clientes:
-        for cliente in lista_total:
-            lista_clientes.write(f"{cliente.qnt_filhos}, {cliente.salarios}\n")
-        
-    with open(nome_arquivo_cliente, "r") as arquivo_clientes:
-        for linha in arquivo_clientes:
-            qnt_filhos, salarios = linha.strip() .split(",")
-            lista_total.append(Cliente(qnt_filhos=int(qnt_filhos), salarios=float(salarios)))
-
-        lista_clientes.close()
-        
-    return lista_clientes, arquivo_clientes
 
 
  
